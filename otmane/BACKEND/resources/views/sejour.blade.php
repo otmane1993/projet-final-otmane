35736d70e388031a723951c0b93044734c3497f6
otmane1993 <otmomks@gmail.com>
2022-02-26 13:10:08 +0100

    image ajustement dans dashboard admin

--- otmane/BACKEND/resources/views/sejour.blade.php
+++ otmane/BACKEND/resources/views/sejour.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div>
     <ul class="sidebar">
-            <img width="100" height="100" src="{{Storage::url('public/files/Logo-agencia.png')}}"/>
+            <img width="150" height="150" src="{{Storage::url('public/files/Logo-agencia.png')}}"/>
         <li>
             <a href="{{route('hotel')}}" class="hotel"><i class="fa fa-hotel"></i>Hotels</a>
         </li>
