b7fd720fd9f0c1e71c08f0b2ccec536790fffe6c
otmane1993 <otmomks@gmail.com>
2022-03-22 10:39:42 +0100

    logout parfait

--- otmane/FRONTEND/otmane/src/Components/Nav.js
+++ otmane/FRONTEND/otmane/src/Components/Nav.js
@@ -37,10 +37,8 @@ function Nav() {
                 setSwitche(true);
             }
         },1000);
-        //console.log('otmane KSAANI');
         if(localStorage.getItem('token'))
         {
-            //setSwitche(true);
             const accessToken=localStorage.getItem('token');
             
             var reqData = {
@@ -49,7 +47,6 @@ function Nav() {
             axios({
                 method: 'post',
                 url: 'http://127.0.0.1:8000/api/user',
-                //withCredentials: true,
                 crossdomain: true,
                 data: Object.keys(reqData).map(function(key) {
                     return encodeURIComponent(key) + '=' + encodeURIComponent(reqData[key])
@@ -61,7 +58,6 @@ function Nav() {
             }
             }).then((res)=>{ 
                 setName(res.data[0].lastname);
-                //setSwitche(true);
             })
             .catch(function (error) {
                
@@ -70,42 +66,13 @@ function Nav() {
     }
     if(redirect)
     {
-        setTimeout(()=>{setRedirect(false)},1);
+        //setTimeout(()=>{setRedirect(false)},1);
+        setRedirect(false)
         return <Navigate to="/"/>;
     }
   return (
     <>
         <header className="header-acceuil">
-            {/*<nav className="navbar navbar-inverse nav-accueil">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <Link className="navbar-brand" to="/"><img src={logo} width="100" height="100"/></Link>
-                    </div>
-                    <ul className="nav navbar-nav navbar-right">
-                        {
-                            //localStorage.getItem('token')
-                            switche
-                            ?
-                            <>
-                                <li>
-                                    <h4>Bonjour {name}</h4>
-                                </li>
-                                <li><Link to="/history"><span className="glyphicon glyphicon-user"></span>History</Link></li>
-                                <li><Link to="/"><span className="glyphicon glyphicon-user"></span>Recherche</Link></li>
-                                <li><button className="btn btn-primary" onClick={logout}>Deconnexion</button></li>
-                                <li>
-                                <Link to="/modify"><span className="glyphicon glyphicon-user"></span>Modify</Link>
-                                </li>
-                            </>
-                            :
-                            <>
-                                <li><Link to="/register"><span className="glyphicon glyphicon-user"></span> Sign Up</Link></li>
-                                <li><Link to="/login"><span className="glyphicon glyphicon-log-in"></span> Login</Link></li>
-                            </>
-                        }
-                    </ul>
-                </div>
-                    </nav>*/}
             <nav className={(showNave)?'heigh':''}>
                 <input type="checkbox" id="check"/>
                 <label for="check" className="check-btn">
@@ -114,7 +81,6 @@ function Nav() {
                 <Link className="navbar-brand" to="/"><img src={logo} width="100" height="100"/></Link>
                 <ul className={(showNave)?'show':''}>
                 {
-                    //localStorage.getItem('token')
                     switche
                     ?
                     <>
